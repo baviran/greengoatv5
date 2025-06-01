1fc6f6d9fa4f406375266f4cb63aa1e03caadd2f
aviranb <aviranb@jfrog.com>
2025-06-01 11:30:41 +0300

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,11 @@ import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
       <html lang="en"
-            // className="dark"
+            className="dark"
       >
-      <body className="bg-background text-foreground">{children}</body>
+      <body className="bg-background text-foreground ">
+
+      {children}</body>
       </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
-
 export default function Home() {
   return (
-   <div>hi</div>
-  );
+
+      <>
+        <div>jjjj</div>
+      <div className="p-6 bg-card text-card-foreground rounded border border-border">
+          <h1 className="text-xl font-semibold">Welcome</h1>
+          <p className="text-muted">Start building your app</p>
+          <button className="mt-4 bg-primary text-primary-foreground px-4 py-2 rounded">
+              Get Started
+          </button>
+      </div>
+     </>
+    );
 }
